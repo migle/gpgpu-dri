302eab3cb430124a97dab528cc585b0d27d8afbd
Miguel Lopes Santos Ramos <mail@miguel.ramos.name>
2012-06-15 14:36:07 +0100

    Relocations, first attempt.

--- radeon/radeon_gem_command_stream.cpp
+++ radeon/radeon_gem_command_stream.cpp
@@ -11,17 +11,29 @@
 
 using namespace std;
 
-std::atomic<uint32_t> radeon_gem_command_stream::_unused_id;
+std::atomic<uint32_t> radeon_gem_command_stream::_used_id(0);
 
 uint32_t radeon_gem_command_stream::new_id()
 {
-    return _unused_id++;
+    for (uint32_t id = 1; id != 0; id <<= 1)
+        if ((_used_id.fetch_or(id) & id) == 0)
+            return id;
+    throw std::bad_alloc();
 }
 
-radeon_gem_command_stream::radeon_gem_command_stream(
-        radeon_device const& dev)
+void radeon_gem_command_stream::release_id(uint32_t id)
+{
+    _used_id &= ~id;
+}
+
+radeon_gem_command_stream::radeon_gem_command_stream(radeon_device const& dev)
     : gem_command_stream(dev), _id(new_id())
 {
+}
+
+radeon_gem_command_stream::~radeon_gem_command_stream() throw()
+{
+    release_id(_id);
 }
 
 void radeon_gem_command_stream::emit()
@@ -29,15 +41,14 @@ void radeon_gem_command_stream::emit()
     // Two chunks: the instruction buffer and the relocations.
     drm_radeon_cs_chunk chunks[2];
 
-    uint32_t* ib_base = &_ib.front();
     chunks[0].chunk_id = RADEON_CHUNK_ID_IB;
     chunks[0].length_dw = _ib.size();
-    chunks[0].chunk_data = reinterpret_cast<uintptr_t>(ib_base);
+    chunks[0].chunk_data = _ib.empty() ? 0 : reinterpret_cast<uintptr_t>(&_ib.front());
     chunks[1].chunk_id = RADEON_CHUNK_ID_RELOCS;
-    chunks[1].length_dw = _ib.size();
-    chunks[1].chunk_data = reinterpret_cast<uintptr_t>(ib_base);
+    chunks[1].length_dw = _relocs.size() * sizeof(drm_radeon_cs_reloc) / sizeof(uint32_t);
+    chunks[1].chunk_data = _relocs.empty() ? 0 : reinterpret_cast<uintptr_t>(&_relocs.front());
 
-    // The pointers to these two chunks are sent indirectly.
+    // The pointers to these two chunks are with triple indirection (!)
     uint64_t chunk_array[2];
 
     chunk_array[0] = reinterpret_cast<uintptr_t>(&chunks[0]);
@@ -62,5 +73,36 @@ void radeon_gem_command_stream::emit()
     } while (r == -1 && (errno == EINTR || errno == EAGAIN));
     if (r == -1)
         throw system_error(error_code(errno, system_category()), "DRM_IOCTL_RADEON_CS");
+}
 
+void radeon_gem_command_stream::write_reloc(
+        gem_buffer_object const& bo,
+        uint32_t read_domains,
+        uint32_t write_domain,
+        uint32_t flags)
+{
+    /// This function will make sure that a single relocation record is
+    /// sent to the kernel for each distinct buffer object.
+    std::unordered_map<uint32_t, uint32_t>::iterator
+        p = _reloc_map.find(bo.handle());
+
+    if (p == _reloc_map.end())
+    {
+        drm_radeon_cs_reloc reloc;
+        reloc.handle = bo.handle();
+        reloc.read_domains = read_domains;
+        reloc.write_domain = write_domain;
+        reloc.flags = flags;
+        _relocs.push_back(reloc);
+        p = _reloc_map.insert(make_pair(bo.handle(), uint32_t(_relocs.size() - 1))).first;
+    }
+    else
+    {
+        _relocs[p->second].read_domains |= read_domains;
+        _relocs[p->second].write_domain |= write_domain;
+        _relocs[p->second].flags |= flags;
+    }
+
+    write(0xc0001000);
+    write(p->second);
 }
--- radeon/radeon_gem_command_stream.hpp
+++ radeon/radeon_gem_command_stream.hpp
@@ -1,55 +1,84 @@
 #pragma once
 
+#include "gem_buffer_object.hpp"
 #include "gem_command_stream.hpp"
 #include "radeon_device.hpp"
 
-#include <atomic>
 #include <cstdint>
+#include <atomic>
+#include <unordered_map>
 #include <vector>
 
 #include <xf86drm.h>
 #include <drm.h>
 #include <radeon_drm.h>
 
-/// This class wraps a handle to a GEM buffer object of the radeon driver.
+/// This class wraps an in-memory GEM command stream.
 class radeon_gem_command_stream : public gem_command_stream {
 public:
-    /// This constructor creates a GEM buffer object on a radeon device.
+    /// This constructor creates a command stream for a radeon device.
+    /// This command stream is created in memory and when full, it can be
+    /// sent for execution (emited) through the GEM.
     /// \param device The DRI device on which to create the object.
-    /// \param size The BO size in bytes.
-    /// \param alignment The BO alignment in bytes.
-    /// \param domains The initial read/write domains of the BO.
-    /// \param flags The BO creation flags.
-    radeon_gem_command_stream(
-        radeon_device const& device);
+    radeon_gem_command_stream(radeon_device const& device);
+    /// The destructure releases resources associated with the command stream.
+    ~radeon_gem_command_stream() throw();
 
+    /// Emit the command stream for execution.
     void emit();
 
     /// Append a double word to the end of the instruction buffer.
     /// \param data 32-bit word to append.
-    void write(uint32_t data) { _ib.push_back(data); }
-
+    void write(std::uint32_t data)
+    {
+        if (_ib.empty())
+            _ib.reserve(0x200);
+        _ib.push_back(data);
+    }
     /// Append data to the end of the instruction buffer.
     /// The size of the data is measured in number of 32-bit words.
     /// \param p Pointer to the data to append to the IB.
     /// \param n Number of double words to append.
     void write(void* p, std::size_t n)
     {
-        uint32_t* ptr = static_cast<uint32_t*>(p);
+        if (_ib.empty())
+            _ib.reserve((n + 0x1ffu) & ~0x1ffu);
+        std::uint32_t* ptr = static_cast<std::uint32_t*>(p);
         _ib.insert(_ib.end(), ptr, ptr + n);
+    }
+    /// Append a relocation packet to the end of the instruction buffer.
+    void write_reloc(
+        gem_buffer_object const& bo,
+        uint32_t read_domains = 0,
+        uint32_t write_domain = 0,
+        uint32_t flags = 0);
+
+    /// iostream-like output operator for appending a dword to the end of the
+    /// instruction buffer.
+    /// \param cs The command stream to write to.
+    /// \param dw The dword to write.
+    /// \returns The same command stream, cs.
+    friend radeon_gem_command_stream& operator << (radeon_gem_command_stream& cs, std::uint32_t dw)
+    {
+        cs.write(dw);
+        return cs;
     }
 
 protected:
-    /// This allows us to generate a distinct id to each command stream.
-    static std::atomic<uint32_t> _unused_id;
-    /// Generate a new id for a command stream.
-    static uint32_t new_id();
+    /// This mask allows us to generate a distinct id for each command stream.
+    static std::atomic<std::uint32_t> _used_id;
+    /// This function generates an unused id for a command stream.
+    static std::uint32_t new_id();
+    /// This function releases the id of a command stream.
+    static void release_id(std::uint32_t);
 
 protected:
-    /// The relocation chunk.
-    std::vector<uint32_t> _relocs;
     /// The instruction buffer chunk.
-    std::vector<uint32_t> _ib;
-    /// The id of this command stream.
-    uint32_t _id;
+    std::vector<std::uint32_t> _ib;
+    /// The relocations chunk.
+    std::vector<drm_radeon_cs_reloc> _relocs;
+    /// Maps an buffer object handle to an index in the relocations chunk.
+    std::unordered_map<std::uint32_t,std::uint32_t> _reloc_map;
+    /// The unique id of this command stream.
+    std::uint32_t _id;
 };
